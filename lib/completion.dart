0b77f2304af7d418c6a1708046cf30a4d16f44fc
Kevin Moore <github@j832.com>
2014-03-05 07:53:14 -0800

    removing deprecated git and completion

--- lib/completion.dart
+++
@@ -1,7 +0,0 @@
-/**
- * Use the `completion` package instead.
- */
-@deprecated
-library completion;
-
-export 'package:completion/completion.dart';
--- test/harness_console.dart
+++ test/harness_console.dart
@@ -1,21 +1,11 @@
 library harness_console;
 
 import 'package:unittest/unittest.dart';
-import 'package:unittest/vm_config.dart';
 
 import 'bot_io/_bot_io.dart' as bot_io;
-import 'bot_git/_bot_git.dart' as bot_git;
-import 'completion/_completion.dart' as completion;
 
-main() {
-  testCore(new VMConfiguration());
-}
-
-void testCore(Configuration config) {
-  unittestConfiguration = config;
+void main() {
   groupSep = ' - ';
 
   bot_io.main();
-  bot_git.main();
-  completion.main();
 }
