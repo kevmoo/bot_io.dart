29dfbe62441cd7ea2620eed38e0a643790bafdff
Kevin Moore <github@j832.com>
2014-03-05 07:55:54 -0800

    flatten test structure

--- test/harness_console.dart
+++ test/harness_console.dart
@@ -2,10 +2,20 @@ library harness_console;
 
 import 'package:unittest/unittest.dart';
 
-import 'bot_io/_bot_io.dart' as bot_io;
+import 'entity_populater_tests.dart' as entity_populater;
+import 'sha_file_tests.dart' as sha;
+import 'temp_dir_tests.dart' as temp_dir;
+import 'update_directory_tests.dart' as update_directory;
 
 void main() {
   groupSep = ' - ';
 
-  bot_io.main();
+  group('temp dir and validate', temp_dir.main);
+
+  group('sha1 file tests', sha.main);
+
+  group('EntityPopulator', () {
+    entity_populater.main();
+    update_directory.main();
+  });
 }
